cb862233cf61bb62e49d3f5b99318531485d4786
ricardo <ricardo_fabbro@hotmail.com>
2023-06-27 23:18:09 -0300

    projeto

---
+++ src/pages/Details/index.jsx
@@ -0,0 +1,27 @@
+import { Container, Links } from "./styles"
+
+import { Header } from "../../components/Header"
+import { Button } from "../../components/Button"
+import { Section } from "../../components/Section"
+
+export function Details() {
+    
+  return (
+    <Container>
+
+      <Header />
+
+      <Section title="Links úteis">
+          <Links>
+              <li><a href="#">http://www.rocketseat.com.br</a></li>
+              <li><a href="#">http://www.rocketseat.com.br</a></li>
+           </Links>
+      </Section>
+
+      <Button title="Voltar"/>
+       
+    </Container>
+   
+   )
+}
+
